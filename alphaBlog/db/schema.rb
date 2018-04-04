a605bf3126ec4c8820bc248686d61f4bc4f0d3e5
vboparai <vboparai11@gmail.com>
2018-04-04 04:33:00 -0400

    new commit

--- alphaBlog/db/schema.rb
+++ alphaBlog/db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180330163729) do
+ActiveRecord::Schema.define(version: 20180404053802) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
